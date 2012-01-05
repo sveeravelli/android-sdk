7154c0f46cf0900d3bae1b996230aa644ef5c660
Mikhail Ulinich <mikhail@ooyala.com>
2012-01-04 20:50:24 -0800

    Removed some unused imports

--- sdk/src/com/ooyala/android/AdSpot.java
+++ sdk/src/com/ooyala/android/AdSpot.java
@@ -3,11 +3,7 @@ package com.ooyala.android;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Set;
 
 import org.json.*;
 
