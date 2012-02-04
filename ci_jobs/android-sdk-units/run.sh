4c0abb388bb4d41b9df3e030ac526a6c791da05a
Jigish Patel <jigish@ooyala.com>
2012-02-03 16:58:03 -0800

    add git pull

--- ci_jobs/android-sdk-units/run.sh
+++ ci_jobs/android-sdk-units/run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+CURRDIR=`pwd`
+
 SCRIPTDIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASEDIR=${SCRIPTDIR}/../../
 
+cd ${BASEDIR}
+git pull
+
 ${BASEDIR}/script/android-sdk tests || exit $?
+
+cd ${CURRDIR}
