183a20dd9201ab115f07730f9309cdc895ab8944
Chris Leonavicius <chrisl@ooyala.com>
2013-07-15 14:19:19 -0700

    Fix tests

--- sdk/tests/src/com/ooyala/android/OoyalaAPIHelperTest.java
+++ sdk/tests/src/com/ooyala/android/OoyalaAPIHelperTest.java
@@ -14,49 +14,17 @@ public class OoyalaAPIHelperTest extends AndroidTestCase {
     super();
   }
 
-  protected void setUp() {
-    apiHelper = new OoyalaAPIHelper(TestConstants.TEST_API_KEY, TestConstants.TEST_SECRET);
-  }
+  protected void setUp() {}
 
   protected void tearDown() {}
-
-  public void testJSONForSecureAPI() {
-    Map<String, String> params = new HashMap<String, String>();
-    params.put("device", "android");
-    params.put("domain", "www.ooyala.com");
-    // TODO once we push SAS, go to player.ooyala.com
-    String json = apiHelper.jsonForSecureAPI("http://player.ooyala.com",
-        "/sas/player_api/v1/authorization/embed_code/" + TestConstants.TEST_PCODE + "/"
-            + TestConstants.TEST_VIDEO, params);
-    assertTrue(json.contains("\"authorized\":true"));
-  }
-
-  public void testObjectForSecureAPI() {
-    Map<String, String> params = new HashMap<String, String>();
-    params.put("device", "android");
-    params.put("domain", "www.ooyala.com");
-    // TODO once we push SAS, go to player.ooyala.com
-    JSONObject json = apiHelper.objectForSecureAPI("http://player.ooyala.com",
-        "/sas/player_api/v1/authorization/embed_code/" + TestConstants.TEST_PCODE + "/"
-            + TestConstants.TEST_VIDEO, params);
-    assertFalse(json.isNull("authorization_data"));
-    try {
-      assertFalse(json.getJSONObject("authorization_data").isNull(TestConstants.TEST_VIDEO));
-      assertTrue(json.getJSONObject("authorization_data").getJSONObject(TestConstants.TEST_VIDEO)
-          .getBoolean("authorized"));
-    } catch (Exception e) {
-      e.printStackTrace();
-      fail();
-    }
-  }
 
   public void testJSONForAPI() {
     Map<String, String> params = new HashMap<String, String>();
     params.put("device", "android");
-    String json = OoyalaAPIHelper
-        .jsonForAPI("http://player.ooyala.com", "/player_api/v1/content_tree/embed_code/"
+    JSONObject json = OoyalaAPIHelper
+        .objectForAPI("http://player.ooyala.com", "/player_api/v1/content_tree/embed_code/"
             + TestConstants.TEST_PCODE + "/" + TestConstants.TEST_VIDEO, params);
-    assertTrue(json.contains("\"embed_code\":\"" + TestConstants.TEST_VIDEO + "\""));
+    assertTrue(json.toString().contains("\"embed_code\":\"" + TestConstants.TEST_VIDEO + "\""));
   }
 
   public void testObjectForAPI() {
