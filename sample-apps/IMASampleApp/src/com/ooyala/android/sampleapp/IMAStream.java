5d4d9d0ccb403491309dee12249d682d1ab93a31
michael.len <michael.len@ooyala.com>
2013-07-17 18:34:41 -0700

    some code cleanup of IMA Stream

--- sample-apps/IMASampleApp/src/com/ooyala/android/sampleapp/IMAStream.java
+++ sample-apps/IMASampleApp/src/com/ooyala/android/sampleapp/IMAStream.java
@@ -1,17 +1,15 @@
 package com.ooyala.android.sampleapp;
 
-import org.w3c.dom.Element;
 
 import com.ooyala.android.Stream;
 
 public class IMAStream extends Stream {
-  /** if this stream must maintain the aspect ratio */
-  private boolean _maintainAspectRatio;
   /**
    * Initialize a Stream using the specified VAST MediaFile XML (subclasses should override this)
    * @param data the Element containing the xml to use to initialize this Stream
    */
   IMAStream(String url) {
+    super();
     this._deliveryType = "mp4";
     this._urlFormat = "text";
     this._url = url;
