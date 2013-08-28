4d11516d836100e37a252d697d0b336a405a5188
michael.len <michael.len@ooyala.com>
2013-08-28 12:14:44 -0700

    [PBK-193] allow partial override of defaultPlayerInfo
    now you can just extend DefaultPlayerInfo, instead of recreating a PlayerInfo

--- sdk/src/com/ooyala/android/StreamPlayer.java
+++ sdk/src/com/ooyala/android/StreamPlayer.java
@@ -11,46 +11,7 @@ import android.os.Handler;
 import android.os.Message;
 
 public abstract class StreamPlayer extends Player {
-  public static PlayerInfo defaultPlayerInfo = new PlayerInfo() {
-      @Override
-      public Set<String> getSupportedProfiles() {
-        return null;
-      }
-
-      @Override
-      public Set<String> getSupportedFormats() {
-        HashSet<String> supportedFormats = new HashSet<String>();
-        supportedFormats.add("mp4");
-        supportedFormats.add("wv_mp4");
-
-        if (OoyalaPlayer.enableHLS || OoyalaPlayer.enableCustomHLSPlayer) {
-          supportedFormats.add("m3u8");
-        }
-
-        if (Build.VERSION.SDK_INT >= Constants.SDK_INT_ICS) {
-          supportedFormats.add("m3u8");
-          supportedFormats.add("wv_wvm");
-          supportedFormats.add("wv_hls");
-        }
-
-        return supportedFormats;
-      }
-    
-      @Override
-      public int getMaxWidth() { return -1; }
-
-      @Override
-      public int getMaxHeight() { return -1; }
-
-      @Override
-      public int getMaxBitrate() { return -1; }
-
-      @Override
-      public String getDevice() { return "android_html"; }
-
-      @Override
-      public String getUserAgent() { return null; }
-    };
+  public static PlayerInfo defaultPlayerInfo = new DefaultPlayerInfo();
 
   private PlayerInfo customPlayerInfo;
   public PlayerInfo getPlayerInfo() {
