bd7ebc99df9980ea8ffd7b7c260a32b75e14a7cf
michael.len <michael.len@ooyala.com>
2013-10-07 22:55:36 -0700

    [PBK-207] (Also PB-2343) Wrap Network pings in an async task
    URL connections shouldn't be done on main thread, and in fact crash Hook because Hook uses Android's Strictmode

--- sdk/src/com/ooyala/android/NetUtils.java
+++ sdk/src/com/ooyala/android/NetUtils.java
@@ -4,17 +4,33 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import android.os.AsyncTask;
 import android.util.Log;
+
+class PingTask extends AsyncTask<URL, Void, Void> {
+
+  protected void onPostExecute() {
+      return;
+  }
+
+  @Override
+  protected Void doInBackground(URL... params) {
+    for (URL url : params) {
+      try {
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.connect();
+        conn.getInputStream();
+      } catch (Exception e) {
+        Log.e(NetUtils.class.getName(), "Ping failed!!!");
+      }
+    }
+    return null;
+  }
+}
 
 class NetUtils {
   public static void ping(URL url) {
     if (url == null) { return; }
-    try {
-      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-      conn.connect();
-      conn.getInputStream();
-    } catch (IOException e) {
-      Log.e(NetUtils.class.getName(), "Ping failed!!!");
-    }
+    new PingTask().execute(url);
   }
 }
