31bfe0f2c1fa896c39d31eb63a091040a5b7a4b6
Hannah Kang <hannah.kang@ooyala.com>
2013-10-16 11:15:32 -0700

    [Internal] Add Channels to InternalTestAppActivity and create a next video button when Channels are loaded.

--- internal-apps/InternalTestApp/src/com/ooyala/test/cases/PlaybackInternalTestAppActivity.java
+++ internal-apps/InternalTestApp/src/com/ooyala/test/cases/PlaybackInternalTestAppActivity.java
@@ -1,8 +1,13 @@
 package com.ooyala.test.cases;
 
 import com.ooyala.test.BaseInternalTestAppActivity;
+import com.ooyala.test.R;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 public class PlaybackInternalTestAppActivity extends BaseInternalTestAppActivity {
 
@@ -12,6 +17,7 @@ public class PlaybackInternalTestAppActivity extends BaseInternalTestAppActivity
 //  OoyalaIMAManager imaManager;
 //  OoyalaPlayer player;
 //  Spinner embedSpinner;
+  Button nextVideo;
 
   /**
    * Called when the activity is first created.
@@ -25,6 +31,9 @@ public class PlaybackInternalTestAppActivity extends BaseInternalTestAppActivity
     embedMap.put("MP4 Video",    "h4aHB1ZDqV7hbmLEv4xSOx3FdUUuephx");
     embedMap.put("VOD with CCs", "92cWp0ZDpDm4Q8rzHfVK6q9m6OtFP-ww");
     embedMap.put("WV-MP4",       "N3ZnF1ZDo2cUf0JIIFMaxv-gKgmF6Dvv");
+    embedMap.put("Channel",      "ozNTJ2ZDqvPWyXTriQF_Ovcd1VuKHGdH");
+    embedMap.put("Channel with pre-roll",    "FncDB0YTrvdMGK3Sva1NUmeQMuB33wbV");
+    embedMap.put("Multiple Google IMA",      "4wbjhoYTp6oRqD6lslRn0xYVTbm2GBzh");
 
     //Update the spinner with the embed map
     embedAdapter.addAll(embedMap.keySet());
@@ -32,4 +41,34 @@ public class PlaybackInternalTestAppActivity extends BaseInternalTestAppActivity
 
   }
 
+  @Override
+  public void onClick(View v) {
+    if (v.getId() == R.id.setButton) {
+      if (embedSpinner.getSelectedItem() == "Channel" || embedSpinner.getSelectedItem() == "Channel with pre-roll"
+          || embedSpinner.getSelectedItem() == "Multiple Google IMA") {
+        //If Channel is selected, add nextVideo button to controlsLayout if it doesn't already exist
+        if (nextVideo == null) {
+          nextVideo = new Button(this);
+          nextVideo.setText("Next Video");
+          nextVideo.setOnClickListener(this);
+          nextVideo.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+
+          LinearLayout controlsLayout = (LinearLayout) findViewById(R.id.controlsLayout);
+          controlsLayout.addView(nextVideo, 0);
+        }
+      } else {
+        //Otherwise, if nextVideo button exists, remove it from controlsLayout
+        if (nextVideo != null) {
+          LinearLayout controlsLayout = (LinearLayout) findViewById(R.id.controlsLayout);
+          controlsLayout.removeView(nextVideo);
+          nextVideo = null;
+        }
+      }
+      super.onClick(v);
+    }
+    else {
+      //If nextVideo button was clicked on, play the next video
+      player.nextVideo(0);
+    }
+  }
 }
