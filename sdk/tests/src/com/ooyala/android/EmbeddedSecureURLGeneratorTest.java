8d1041e38e54e5e9d497f8ed4537745ed041b774
Zhihui Chen <zchen@ooyala.com>
2014-05-02 11:03:07 -0700

    [Internal][UnitTest] replace hard coded string with test constants.

--- sdk/tests/src/com/ooyala/android/EmbeddedSecureURLGeneratorTest.java
+++ sdk/tests/src/com/ooyala/android/EmbeddedSecureURLGeneratorTest.java
@@ -20,7 +20,9 @@ public class EmbeddedSecureURLGeneratorTest extends AndroidTestCase {
   protected void tearDown() {}
 
   public void testSecureURL() {
-    String expecedSecureURL = "http://hello.com/test/path?api_key=l1am06xhbSxa0OtyZsBTshW2DMtp.6Le2A&device=android_sdk&expires=1234567890&signature=wiZiqAdTUnTPXiAR6yBj77%2BN%2BC4u9J8zCiFOtmmQcqQ";
+    String expecedSecureURL = "http://hello.com/test/path?api_key="
+        + TestConstants.TEST_API_KEY
+        + "&device=android_sdk&expires=1234567890&signature=wiZiqAdTUnTPXiAR6yBj77%2BN%2BC4u9J8zCiFOtmmQcqQ";
     Map<String, String> params = new HashMap<String, String>();
     params.put(Constants.KEY_DEVICE, Constants.DEVICE_ANDROID_SDK);
     params.put(Constants.KEY_EXPIRES, "1234567890");
