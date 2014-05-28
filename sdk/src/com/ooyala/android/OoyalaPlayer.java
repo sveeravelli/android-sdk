4e704e1498c1e0e55a963b4b0c5a3e8f0219eeee
Michael Len <michael.len@ooyala.com>
2014-05-27 18:15:58 -0700

    [PBK-404][Playready] Prepare for Epsilon Release

--- sdk/src/com/ooyala/android/OoyalaPlayer.java
+++ sdk/src/com/ooyala/android/OoyalaPlayer.java
@@ -46,7 +46,7 @@ public class OoyalaPlayer extends Observable implements Observer,
    * NOTE[jigish] do NOT change the name or location of this variable without
    * changing pub_release.sh
    */
-  static final String SDK_VERSION = "2.4.0_RC2";
+  static final String SDK_VERSION = "2.4.0_RC2_Playready_Epsilon";
   static final String API_VERSION = "1";
 
   public static enum ActionAtEnd {
