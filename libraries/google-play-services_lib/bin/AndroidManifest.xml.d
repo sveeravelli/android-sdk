b4eb9e7356eee51f8ebf9840246629ea6971a817
Jon Slenk <jonslenk@ooyala.com>
2014-06-06 19:18:05 -0700

    [PBA-243] rm things that shouldn't have been checked in.

--- libraries/google-play-services_lib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/jonslenk/Dev/repos/android-sdk2/libraries/google-play-services_lib/bin/AndroidManifest.xml : \
-/Users/jonslenk/Dev/repos/android-sdk2/libraries/google-play-services_lib/AndroidManifest.xml \
