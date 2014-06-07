26965748f526c7e47d96935c8730e5ecd0140b80
Michael Len <michael.len@ooyala.com>
2014-06-06 18:29:45 -0700

    [Internal] [Playready] Fix Script before we generate Old VisualOn Libraries

--- script/generate_visualon_zip.sh
+++ script/generate_visualon_zip.sh
@@ -10,6 +10,7 @@ cd ${BASE_DIR}
 mkdir ${ZIP_BASE}
 cp -r ${BASE_DIR}/vendor/SecurePlayer/assets ${BASE_DIR}/${ZIP_BASE}/
 cp -r ${BASE_DIR}/vendor/SecurePlayer/GENERAL_ANDR_VOP_PROB_RC_02_00_208_1168/SecurePlayerSDK/libs ${BASE_DIR}/${ZIP_BASE}/
+cp -r ${BASE_DIR}/vendor/SecurePlayer/General/SecurePlayerSDK/libs/armeabi-v7a/* ${BASE_DIR}/${ZIP_BASE}/libs/armeabi-v7a/
 cp -r ${BASE_DIR}/vendor/SecurePlayer/HOW_TO_INTEGRATE_WITH_VISUALON.txt ${BASE_DIR}/${ZIP_BASE}/
 cp -r ${BASE_DIR}/internal-apps/PlayreadyDeviceManagementSampleApp ${BASE_DIR}/${ZIP_BASE}/
 
