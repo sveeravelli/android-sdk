0910431b3b5b864873dac7ed5e73e5d2f0dd08e4
Zhihui Chen <zchen@ooyala.com>
2014-10-27 11:16:38 -0700

    [PBA-795] Fix indentation.

--- sdk/src/com/ooyala/android/LocalizationSupport.java
+++ sdk/src/com/ooyala/android/LocalizationSupport.java
@@ -4,22 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public final class LocalizationSupport {
-	private static Map<String, Map<String, String>> defaultLocales = null;
-	private static Map<String, String> currentLocalizedStrings = null;
+  private static Map<String, Map<String, String>> defaultLocales = null;
+  private static Map<String, String> currentLocalizedStrings = null;
 
-	private static void createDefaultLocales() {
-		final Map<String, String> en_US = new HashMap<String, String>();
-		en_US.put("LIVE", "LIVE");
-		en_US.put("Languages", "Languages");
-		en_US.put("Presentation Styles", "Presentation Styles");
-		en_US.put("Roll-Up", "Roll-Up");
-		en_US.put("Paint-On", "Paint-On");
-		en_US.put("Pop-On", "Pop-On");
-		en_US.put("Done", "Done");
-		en_US.put("None", "None");
-		en_US.put("Learn More", "Learn More");
+  private static void createDefaultLocales() {
+    final Map<String, String> en_US = new HashMap<String, String>();
+    en_US.put("LIVE", "LIVE");
+    en_US.put("Languages", "Languages");
+    en_US.put("Presentation Styles", "Presentation Styles");
+    en_US.put("Roll-Up", "Roll-Up");
+    en_US.put("Paint-On", "Paint-On");
+    en_US.put("Pop-On", "Pop-On");
+    en_US.put("Done", "Done");
+    en_US.put("None", "None");
+    en_US.put("Learn More", "Learn More");
 
-		final Map<String, String> ja_JP = new HashMap<String, String>();
+    final Map<String, String> ja_JP = new HashMap<String, String>();
     ja_JP.put("LIVE", "ライブ");
     ja_JP.put("Languages", "言語");
     ja_JP.put("Presentation Styles", "プレゼンテーションのスタイル");
@@ -29,7 +29,7 @@ public final class LocalizationSupport {
     ja_JP.put("Done", "完了");
     ja_JP.put("None", "なし");
     ja_JP.put("Learn More", "詳細はこちら");
-		
+
     final Map<String, String> es = new HashMap<String, String>();
     es.put("LIVE", "En vivo");
     es.put("Languages", "Idioma");
@@ -38,41 +38,49 @@ public final class LocalizationSupport {
     es.put("None", "Ninguno");
     es.put("Learn More", "Más información");
 
-		defaultLocales = new HashMap<String, Map<String, String>>();
-		defaultLocales.put("en_US", en_US);
-		defaultLocales.put("ja_JP", ja_JP);
+    defaultLocales = new HashMap<String, Map<String, String>>();
+    defaultLocales.put("en_US", en_US);
+    defaultLocales.put("ja_JP", ja_JP);
     defaultLocales.put("es", es);
-		currentLocalizedStrings = loadLocalizedStrings("en_US");
-	}
+    currentLocalizedStrings = loadLocalizedStrings("en_US");
+  }
 
-	/**
-	 * Loads a map of localized strings for specified locale
-	 * @param localeId The ID of locale (such as en_US)
-	 */
-	synchronized public static Map<String, String> loadLocalizedStrings(String localeId) {
+  /**
+   * Loads a map of localized strings for specified locale
+   * 
+   * @param localeId
+   *          The ID of locale (such as en_US)
+   */
+  synchronized public static Map<String, String> loadLocalizedStrings(
+      String localeId) {
     if (localeId.contains("es")) {
       localeId = "es";
     }
-		if (defaultLocales == null) createDefaultLocales();
-		return defaultLocales.get(localeId);
-	}
+    if (defaultLocales == null) {
+      createDefaultLocales();
+    }
+    return defaultLocales.get(localeId);
+  }
 
-	/**
-	 * Instructs the player to use localized strings
-	 */
-	synchronized public static void useLocalizedStrings(Map<String, String> localizedStrings) {
+  /**
+   * Instructs the player to use localized strings
+   */
+  synchronized public static void useLocalizedStrings(
+      Map<String, String> localizedStrings) {
     if (defaultLocales == null)
       createDefaultLocales();
     if (localizedStrings != null) {
       currentLocalizedStrings = localizedStrings;
     }
-	}
+  }
 
-	/**
-	 * Returns current localized strings
-	 */
-	synchronized public static String localizedStringFor(String string) {
-		if (currentLocalizedStrings == null) createDefaultLocales();
-		return currentLocalizedStrings.get(string);
-	}
+  /**
+   * Returns current localized strings
+   */
+  synchronized public static String localizedStringFor(String string) {
+    if (currentLocalizedStrings == null) {
+      createDefaultLocales();
+    }
+    return currentLocalizedStrings.get(string);
+  }
 }
