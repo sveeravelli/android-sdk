675022e4adda8e4a9df06664021701b7c312c3ee
Zhihui Chen <zchen@ooyala.com>
2015-01-27 10:44:37 -0800

    [PBA-964] gracefully close the resource.

--- sdk/ooyalaCoreSDK/src/main/java/com/ooyala/android/OoyalaAPIHelper.java
+++ sdk/ooyalaCoreSDK/src/main/java/com/ooyala/android/OoyalaAPIHelper.java
@@ -3,6 +3,7 @@ package com.ooyala.android;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.SocketTimeoutException;
 import java.net.URL;
@@ -63,8 +64,16 @@ class OoyalaAPIHelper {
       }
     } catch (SocketTimeoutException e) {
       DebugMode.logE(TAG, "Connection to " + url.toString() + " timed out.");
-    } catch (Exception e) {
+    } catch (IOException e) {
       e.printStackTrace();
+    } finally {
+      if (rd != null) {
+        try {
+         rd.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
     }
 
     return sb.toString();
