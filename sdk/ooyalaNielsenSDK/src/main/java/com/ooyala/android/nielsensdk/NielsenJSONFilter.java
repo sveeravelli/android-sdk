059b7dde02ada927e80d30eb847300d6a54b4800
Jon Slenk <jonslenk@ooyala.com>
2015-02-04 15:18:40 -0800

    [NielsenAndroidStudio] attempt to migrate our Nielsen SDK to gradle & Android Studio style.

---
+++ sdk/ooyalaNielsenSDK/src/main/java/com/ooyala/android/nielsensdk/NielsenJSONFilter.java
@@ -0,0 +1,35 @@
+package com.ooyala.android.nielsensdk;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.util.Pair;
+
+public class NielsenJSONFilter {
+
+  private Set<Pair<Pattern,String>> psz;
+
+  public NielsenJSONFilter() {
+    this.psz = new HashSet<Pair<Pattern,String>>();
+
+    for( String s : new String[]{ "\\\\b", "\\\\f", "\\\\n", "\\\\r", "\\\\t" } ) {
+      psz.add( new Pair<Pattern,String>( Pattern.compile( "([^\\\\])"+s ), "$1" ) );
+      psz.add( new Pair<Pattern,String>( Pattern.compile( "^"+s ), "" ) );
+    }
+
+    psz.add( new Pair<Pattern,String>( Pattern.compile( "\\\\'" ), "'" ) );
+  }
+
+  public String filter( String json ) {
+    String r = json;
+
+    for( Pair<Pattern,String> ps : psz ) {
+      final Matcher backspaceMatcher = ps.first.matcher( r );
+      r = backspaceMatcher.replaceAll( ps.second );
+    }
+
+    return r;
+  }
+}
