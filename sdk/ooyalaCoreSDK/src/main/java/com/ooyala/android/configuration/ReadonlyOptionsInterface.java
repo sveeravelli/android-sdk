32969f388dc9b96f68f69d8b17edf3474a896fae
Jon Slenk <jonslenk@ooyala.com>
2015-03-20 10:59:58 -0700

    [docs] add docs for some Options/Configuration, and make some fields final in there.

--- sdk/ooyalaCoreSDK/src/main/java/com/ooyala/android/configuration/ReadonlyOptionsInterface.java
+++ sdk/ooyalaCoreSDK/src/main/java/com/ooyala/android/configuration/ReadonlyOptionsInterface.java
@@ -2,22 +2,50 @@ package com.ooyala.android.configuration;
 
 public interface ReadonlyOptionsInterface {
 
+  /**
+   * @see FCCTVRatingConfiguration
+   */
   FCCTVRatingConfiguration getTVRatingConfiguration();
 
+  /**
+   * @see VisualOnConfiguration
+   */
   VisualOnConfiguration getVisualOnConfiguration();
 
+  /**
+   * If "true",  show ad controls during ads playback.
+   */
   boolean getShowAdsControls();
 
+  /**
+   * If "true", show cuepoint markers for ads.
+   */
   boolean getShowCuePoints();
 
-  boolean getPreloadContent();
-
-  boolean getShowPromoImage();
-
+  /**
+   * If "true", show live controls for live content playback (live stream only).
+   */
   boolean getShowLiveControls();
 
+  /**
+   * If "true", load the content when the rquired information and authorization is available.
+   * If "false", load the content after pre-roll (if pre-roll is available).
+   */
+  boolean getPreloadContent();
+
+  /**
+   * If "true", show a promo image if one is available.
+   */
+  boolean getShowPromoImage();
+
+  /**
+   * Network connection timeout value used by networking operations.
+   */
   int getConnectionTimeoutInMillisecond();
 
+  /**
+   * Read timeout value used by networking operations.
+   */
   int getReadTimeoutInMillisecond();
 
 }
