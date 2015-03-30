b37de6e35f31fbd6ca4609ff35275d9d1fa36ad2
Michael Len <michael.len@ooyala.com>
2015-03-30 08:41:05 -0700

    [PBA-1551] Revert one of the version changes for VisualOn
    VisualOn version was not supposed to change in 42f5c462.

--- sdk/ooyalaCoreSDK/src/main/java/com/ooyala/android/visualon/VisualOnStreamPlayer.java
+++ sdk/ooyalaCoreSDK/src/main/java/com/ooyala/android/visualon/VisualOnStreamPlayer.java
@@ -59,7 +59,7 @@ FileDownloadCallback, PersonalizationCallback, AcquireRightsCallback{
   private static boolean didCleanupLocalFiles = false;
   private static final String TAG = "VisualOnStreamPlayer";
   private static final String DISCREDIX_MANAGER_CLASS = "com.discretix.drmdlc.api.DxDrmDlc";
-  private static final String EXPECTED_VISUALON_VERSION = "3.14.12-B76704";
+  private static final String EXPECTED_VISUALON_VERSION = "3.13.0-B71738";
   private static final String EXPECTED_SECUREPLAYER_VO_VERSION = "3.14.12-B76704";
   private VisualOnConfiguration _visualOnConfiguration = null;
   private static final boolean ENABLE_DEBUGGING = false;
