4b83cbb0b4b509c4077882144222ac0fdf4eb046
Matthew Chen <zchen@ooyala.com>
2015-04-23 16:42:29 -0700

    Gen Release v4.1.0_RC1

--- sdk/ooyalaCoreSDK/src/main/java/com/ooyala/android/OoyalaPlayer.java
+++ sdk/ooyalaCoreSDK/src/main/java/com/ooyala/android/OoyalaPlayer.java
@@ -72,7 +72,7 @@ public class OoyalaPlayer extends Observable implements Observer,
    * NOTE[jigish] do NOT change the name or location of this variable without
    * changing pub_release.sh
    */
-  static final String SDK_VERSION = "v4.0.1_RC2";
+  static final String SDK_VERSION = "v4.1.0_RC1";
   static final String API_VERSION = "1";
   public static final String PREFERENCES_NAME = "com.ooyala.android_preferences";
 
