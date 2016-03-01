04cb3e1ced444fc64679623d7524bc044942972d
Matthew Chen <zchen@ooyala.com>
2016-03-01 13:59:43 -0800

    [PBA-3392] fix vast ad player for podded ads.

--- sdk/ooyalaCoreSDK/src/main/java/com/ooyala/android/AdPodInfo.java
+++ sdk/ooyalaCoreSDK/src/main/java/com/ooyala/android/AdPodInfo.java
@@ -1,55 +1,58 @@
 package com.ooyala.android;
 
-import java.net.URL;
-
 /**
  * Created by ukumar on 2/4/16.
  */
 public class AdPodInfo {
+  private String title;
+  private String description;
+  private String clickUrl;
+  private int adsCount;
+  private int unplayedCount;
+  private double skipoffset;
+  private boolean adbar;
+  private boolean controls;
 
-    private String title;
-    private String description;
-    private String clickUrl;
-    private int adsCount;
-    private int unplayedCount;
-    private boolean adbar;
-    private boolean controls;
+  public AdPodInfo(String title,String description, String clickUrl, int adsCount, int unplayedCount, double skipoffset, boolean adbar, boolean controls ) {
+    this.title = title;
+    this.description = description;
+    this.clickUrl = clickUrl;
+    this.adsCount = adsCount;
+    this.unplayedCount = unplayedCount;
+    this.skipoffset = skipoffset;
+    this.adbar = adbar;
+    this.controls = controls;
+  }
 
-    public AdPodInfo(String title,String description, String clickUrl, int adsCount, int unplayedCount, boolean adbar, boolean controls ) {
-        this.title = title;
-        this.description = description;
-        this.clickUrl = clickUrl;
-        this.adsCount = adsCount;
-        this.unplayedCount = unplayedCount;
-        this.adbar = adbar;
-        this.controls = controls;
+  public boolean isAdbar() {
+      return adbar;
+  }
+  public String getDescription() {
+      return description;
+  }
 
-    }
-    public boolean isAdbar() {
-        return adbar;
-    }
-    public String getDescription() {
-        return description;
-    }
+  public String getTitle() {
+      return title;
+  }
 
-    public String getTitle() {
-        return title;
-    }
+  public String getClickUrl() {
+      return clickUrl;
+  }
 
-    public String getClickUrl() {
-        return clickUrl;
-    }
+  public int getAdsCount() {
+      return adsCount;
+  }
 
-    public int getAdsCount() {
-        return adsCount;
-    }
+  public int getUnplayedCount() {
+      return unplayedCount;
+  }
 
-    public int getUnplayedCount() {
-        return unplayedCount;
-    }
+  public boolean isControls() {
+      return controls;
+  }
 
-    public boolean isControls() {
-        return controls;
-    }
+  public double getSkipoffset() {
+    return skipoffset;
+  }
 
 }
