81879493e085d8176a6b1535044396107a39a1bc
Jon Slenk <jonslenk@ooyala.com>
2016-03-03 14:28:12 -0800

    [PBA-3404] Add javadoc.

--- sdk/ooyalaCoreSDK/src/main/java/com/ooyala/android/player/PlayerInterfaceUtil.java
+++ sdk/ooyalaCoreSDK/src/main/java/com/ooyala/android/player/PlayerInterfaceUtil.java
@@ -10,10 +10,19 @@ import java.util.Map;
 import static com.ooyala.android.OoyalaPlayer.STATE_CHANGED_NOTIFICATION_NAME;
 import static com.ooyala.android.OoyalaPlayer.State;
 
+/**
+ * Utilities to collect common code around how notifications are generated.
+ */
 public class PlayerInterfaceUtil {
 
   private static final String TAG = PlayerInterfaceUtil.class.getSimpleName();
 
+  /**
+   * Build the OoyalaNotification to send out when state changes.
+   * @param oldState the previous state e.g. PLAYING.
+   * @param newState the new state e.g. PAUSED.
+   * @return OoyalaNotification named STATE_CHANGED_NOTIFICATION_NAME, and data that is Map<String,State> with keys OLD_STATE_KEY and NEW_STATE_KEY.
+   */
   public static final OoyalaNotification buildSetStateNotification(OoyalaPlayer.State oldState, OoyalaPlayer.State newState) {
     DebugMode.logD(TAG, "player set state " + newState + ", old state was " + oldState);
     final Map<String,State> data = new HashMap<String,OoyalaPlayer.State>();
